c767e23e0cde04125236c1b33cd66edfeb54dc0e
Nagaraju <nbende@gmail.com>
2016-04-24 22:53:54 +0530

    AngularJS Demos

---
+++ JSBasics1/code.js
@@ -0,0 +1,6 @@
+﻿/// <reference path="Scripts/angular.js" />
+var x = angular.module("app1", []);
+x.controller("ctrl1", function ctrl1($scope) {
+    var courses = ['ASP.NET MVC', 'ASP.NET Web Forms', 'MS AZURE', 'ASP.NET CORE 1.0'];
+    $scope.CourseList = courses;
+});
