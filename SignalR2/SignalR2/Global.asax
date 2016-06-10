470f3abe0df6a14ff78f35290870b6ee6451146e
Nagaraju <nbende@gmail.com>
2016-06-10 19:31:41 +0530

    SignalR

---
+++ SignalR2/SignalR2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SignalR2.MvcApplication" Language="C#" %>
---
+++ SignalR2/SignalR2/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Owin;//
+using Microsoft.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+[assembly: OwinStartupAttribute(typeof(SignalR2.Startup))]
+namespace SignalR2
+{
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+
+    }
+}
