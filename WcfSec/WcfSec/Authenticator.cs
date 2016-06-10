43589c1f0a320dce9ccececb9de5c08153897a46
Nagaraju <nbende@gmail.com>
2016-06-10 23:48:42 +0530

    security

---
+++ WcfSec/WcfSec/Authenticator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ServiceModel;
+using System.IdentityModel.Selectors;//
+namespace WcfSec
+{
+    public class Authenticator : UserNamePasswordValidator
+    {
+        public override void Validate(string userName, string password)
+        {
+            if (userName != "scott" && password != "tiger")
+            {
+                throw new FaultException("Invalid user and/or password");
+            }
+        }
+
+    }
+}
